58f23c13723aec19e23a761c1c6baf1c9ad8b71f
findhostcoupons@gmail.com <findhostcoupons@gmail.com>
2023-08-02 19:28:04 +0300

    commenting code with user profile image

--- resources/views/components/layouts/porto.blade.php
+++ resources/views/components/layouts/porto.blade.php
@@ -67,11 +67,13 @@
 
 			<!-- start: header -->
 			<header class="header">
+		    
 				<div class="logo-container">
 					<a href="/" class="logo">
 						<img src="{{ asset('img/logo.png') }}" width="75" height="35" alt="Porto Admin" />
 					</a>
-				</div>
+				</div>	
+		 
 
 				<!-- start: search & user box -->
 				<div class="header-right">
@@ -113,9 +115,11 @@
 
 					<div id="userbox" class="userbox">
 						<a href="#" data-bs-toggle="dropdown">
+							<!--
 							<figure class="profile-picture">
 								<img src="img/!logged-user.jpg" alt="Joseph Doe" class="rounded-circle" data-lock-picture="img/!logged-user.jpg" />
 							</figure>
+							-->
 							<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
 								<span class="name">User Name</span>
 								<span class="role">Administrator</span>
