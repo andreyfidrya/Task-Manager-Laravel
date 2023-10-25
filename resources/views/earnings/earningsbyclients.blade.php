bd4cc039a0d8cacccec6302174c67cde7a727732
Andrey Fidrya <findhostcoupons@gmail.com>
2023-10-25 23:47:46 +0300

    Tables for clients

--- resources/views/earnings/earningsbyclients.blade.php
+++ resources/views/earnings/earningsbyclients.blade.php
@@ -1,11 +1,31 @@
 <x-layouts.porto title="Earnings by Clients per Month" header="Earnings by Clients per Month">
-@foreach($clients as $client)
 
-    <strong>{{$client->name}}</strong><br>
+    @foreach($clients as $client)            
+        <strong><h2>{{$client->name}}</h2></strong><br>
+        <table class="table">
+<thead>
+<tr>  
+  <th scope="col">Task</th>
+  <th scope="col">Budget, $USD</th>
+  <th scope="col">Performance</th>
+  <th scope="col">Due date</th>
+  <th scope="col">Author</th>
+  <th scope="col">Completion date</th>         
+</tr>
+</thead>
+<tbody>
+    @foreach($client->tasks()->onlyTrashed()->get() as $task)                
+<tr>
+  <td>{{ $task->task }}</td> 
+  <td>{{ $task->budget }}</td>  
+  <td>{!! $task->performance !!}</td> 
+  <td>{{ $task->duedate }}</td> 
+  <td>{{ $task->author }}</td>
+  <td>{{ $task->deleted_at }}</td> 
+</tr>
+    @endforeach
 
-        @foreach($client->tasks()->onlyTrashed()->get() as $task)
-            {{ $task->task }}<br>
-        @endforeach
-
-@endforeach
+</tbody>
+</table>
+    @endforeach
 </x-layouts.porto>
