58cbe424340402b9ff3363c98264954ec6f6a5e3
Andrey Fidrya <findhostcoupons@gmail.com>
2023-11-02 19:58:44 +0200

    Preparing a table for total monthly spendings

--- resources/views/tasks/trash.blade.php
+++ resources/views/tasks/trash.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.porto title="Total Earnings per Month" header="Total Earnings per Month">
 
-
+<h3>All tasks completed per month:</h3>
 <table class="table">
 <thead>
 <tr>
@@ -42,4 +42,37 @@
 </tbody>
 </table>
 Total earnings per month: <strong>{{$sum}} USD</strong><br>
+<h3>All expenses per month:</h3>
+<p>
+<a href="#" class="btn btn-success">Add an expense</a>
+</p>
+<table class="table">
+<thead>
+<tr>
+  <th scope="col">Spending</th>
+  <th scope="col">Amount, $USD</th> 
+  <th scope="col">Date</th> 
+  <th scope="col">Action</th>       
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td></td> 
+  <td></td> 
+  <td></td> 
+  <td>
+      <form method="post" onClick="return confirm('Do you want to remove the task from forever?')" action="">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">Удалить</button>
+      </form>
+      <form method="post" action="">
+                        @csrf
+                        @method('PUT')
+                        <button class="btn btn-success">Восстановить</button>
+      </form>
+  </td>    
+</tr>
+</tbody>
+</table>
 </x-layouts.porto>
