6e78e38a8497da61042e6bc6ad8d39a440bc27c4
Andrey Fidrya <findhostcoupons@gmail.com>
2023-11-02 22:48:26 +0200

    Finishing Spendings page

--- resources/views/spendings/index.blade.php
+++ resources/views/spendings/index.blade.php
@@ -1,5 +1,4 @@
 <x-layouts.porto title="Total Spendings per Month" header="Total Spendings per Month">
-<h3>All expenses per month:</h3>
 <p>
 <a href="#" class="btn btn-success">Add an expense</a>
 </p>
@@ -18,18 +17,15 @@
   <td></td> 
   <td></td> 
   <td>
-      <form method="post" onClick="return confirm('Do you want to remove the task from forever?')" action="">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger">Удалить</button>
-      </form>
-      <form method="post" action="">
-                        @csrf
-                        @method('PUT')
-                        <button class="btn btn-success">Восстановить</button>
-      </form>
+    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+    <form method="post" action="">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-sm btn-danger" onClick="return confirm('Do you really want to delete the task from ')">Delete</button>
+    </form>
   </td>    
 </tr>
 </tbody>
 </table>
+Spendings per month: <strong></strong><br>
 </x-layouts.porto>
