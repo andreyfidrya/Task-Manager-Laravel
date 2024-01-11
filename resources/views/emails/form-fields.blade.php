7f8647ea2f5d962f774a08331dfdf37c1b18e930
Andrey Fidrya <findhostcoupons@gmail.com>
2024-01-11 23:14:32 +0200

    selecting messages for contract types

--- resources/views/emails/form-fields.blade.php
+++ resources/views/emails/form-fields.blade.php
@@ -15,7 +15,15 @@
     <x-form-select name="seo" label="SEO" :options="['No' => 'No', 'Yes' => 'Yes']" placeholder="Does the job require SEO skills?"/> 
 </div>
 <div class="mb-3">    
-    <x-form-input name="cost" label="Cost"/> 
+    <x-form-select name="contract" onchange="myFunction(event)" label="Contract Type" :options="['My writing speed is 500 words of original content (research + writing) per 1 billable hour.' => 'Hourly', 'My price is ... per 500 words of original content (research + writing).' => 'Fixed Price', 'The budget = ... sounds good to me.' => 'Budget']" placeholder="What is your contract type?"/>
+</div>
+<script>
+function myFunction(e) {
+    document.getElementById("myText").value = e.target.value
+}
+</script>
+<div class="mb-3">    
+    <x-form-input id="myText" name="cost" label="Cost"/> 
 </div>
 <div class="mb-3">
     <x-form-textarea name="conclusion" id="editor2" label="Conclusion"/>
