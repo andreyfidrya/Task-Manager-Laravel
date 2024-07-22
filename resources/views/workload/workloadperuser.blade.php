8ce8c7d10d0ca60b4ef174b90ab20e5c124f6f58
Andrey Fidrya <findhostcoupons@gmail.com>
2024-07-22 18:42:54 +0300

    username for workload

--- resources/views/workload/workloadperuser.blade.php
+++ resources/views/workload/workloadperuser.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.porto title="Workload per user per week" header="Workload per user per week">
+<x-layouts.porto title="Workload per user per week" header="Workload per user per week" username={{$username}}>
 @foreach($users as $user)
 <h3>{{$user->name}} has written <strong> {{ $user->tasks()->onlyTrashed()->whereBetween('deleted_at', [$weekStartDate, $weekEndDate])->sum('wordcount'); }} words per week</h3></strong>
 @endforeach
