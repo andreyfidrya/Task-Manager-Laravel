1078deb6c4798e83c8f826c0318f083058dca118
Andrey Fidrya <findhostcoupons@gmail.com>
2024-08-08 16:24:50 +0300

    dynamic title

--- resources/views/components/layouts/porto.blade.php
+++ resources/views/components/layouts/porto.blade.php
@@ -13,7 +13,7 @@
 		<!-- Basic -->
 		<meta charset="UTF-8">
 
-		<title>{Task Manager Laravel}</title>
+		<title>{title}</title>
 
 		<meta name="keywords" content="HTML5 Admin Template" />
 		<meta name="description" content="Porto Admin - Responsive HTML5 Template">
