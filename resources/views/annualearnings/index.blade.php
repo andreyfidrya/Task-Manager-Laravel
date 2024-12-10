775f56591c51cf846e9457b91b443a8350a725f5
Andrey Fidrya <findhostcoupons@gmail.com>
2024-12-10 19:13:44 +0200

    beginning of Annual Earnings by Months index page

--- resources/views/annualearnings/index.blade.php
+++ resources/views/annualearnings/index.blade.php
@@ -1,3 +1,16 @@
 <x-layouts.porto title="Annual Earnings by Months" header="Annual Earnings by Months" username={{$username}}>
-
+<p>
+<a href="{{ route('annualearnings.create') }}" class="btn btn-success">Add Monthly Earnings</a>
+<button class="btn btn-danger removeAll">Remove Selected Monthly Earnings</button>
+</p>
+<table class="table">
+<thead>
+<tr>
+  <th scope="col"><input type="checkbox" id="checkboxesMain"></th>
+  <th scope="col">Month</th>
+  <th scope="col">Amount, $USD</th> 
+  <th scope="col">Date</th> 
+  <th scope="col">Action</th>       
+</tr>
+</thead>
 </x-layouts.porto>
