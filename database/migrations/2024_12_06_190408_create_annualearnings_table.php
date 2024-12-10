09c5f1bb1d407acfb3d0f072fce1468e133bf4c3
Andrey Fidrya <findhostcoupons@gmail.com>
2024-12-10 19:50:55 +0200

    database for annual earnings by months was created

--- database/migrations/2024_12_06_190408_create_annualearnings_table.php
+++ database/migrations/2024_12_06_190408_create_annualearnings_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('annualearnings', function (Blueprint $table) {
             $table->id();
+            $table->string('month');
+            $table->float('amount');
             $table->timestamps();
         });
     }
