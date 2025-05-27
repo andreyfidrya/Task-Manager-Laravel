0513887d33fe18e911bf6e54cd1f4f84a7f7ae44
Andrey Fidrya <findhostcoupons@gmail.com>
2025-05-27 12:29:26 +0300

    a fix for Total Earnings per Month

--- resources/views/tasks/trash.blade.php
+++ resources/views/tasks/trash.blade.php
@@ -1,5 +1,8 @@
 <x-layouts.porto title="Total Earnings per Month" header="Total Earnings per Month" username={{$username}}>
-
+Earnings per month: <strong>{{$sum}} USD</strong><br>
+Spendings per month: <strong>{{ $sumspent }} USD</strong><br>
+Total amount of vat per month: <strong>{{ $sumvat }} USD</strong><br>
+<h2>Profits per month: <strong>{{ $sum - $sumspent - $sumvat }} USD</strong><br></h2>
 <p>
 <button class="btn btn-danger removeAll">Remove Selected Tasks</button>
 </p>
@@ -44,10 +47,7 @@
 
 </tbody>
 </table>
-Earnings per month: <strong>{{$sum}} USD</strong><br>
-Spendings per month: <strong>{{ $sumspent }} USD</strong><br>
-Total amount of vat per month: <strong>{{ $sumvat }} USD</strong><br>
-<h2>Profits per month: <strong>{{ $sum - $sumspent - $sumvat }} USD</strong><br></h2> 
+ 
 </x-layouts.porto>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
