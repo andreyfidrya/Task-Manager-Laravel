42d532d246897759c0cc8087202e90c9f02e739f
Andrey Fidrya <findhostcoupons@gmail.com>
2025-07-17 15:47:56 +0300

    slug automation for topics

--- resources/views/emails/topics/form-fields.blade.php
+++ resources/views/emails/topics/form-fields.blade.php
@@ -1,7 +1,24 @@
 @csrf
 <div class="mb-3">    
-    <x-form-input name="slug" label="Slug"/> 
-</div>
-<div class="mb-3">    
     <x-form-input name="name" label="Name"/> 
 </div>
+<div class="mb-3">    
+    <x-form-input name="slug" label="Slug"/> 
+</div>
+
+<script>
+    $(function(){            
+
+        $("input[name='name']").on("change",function(){
+            $("input[name='slug']").val(StringToSlug($(this).val())); 
+        });
+
+    });
+
+    function StringToSlug(Text)
+    {
+        return Text.toLowerCase()
+        .replace(/[^\w ]+/g,"")
+        .replace(/ +/g,"-");
+    }
+</script>
