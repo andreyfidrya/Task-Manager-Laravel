c411a395f56e5f36b43fe7cf6f82c40f7d6b9d8b
Andrey Fidrya <findhostcoupons@gmail.com>
2025-07-26 16:44:30 +0300

    Profile image for workload

--- resources/views/workload/totalworkload.blade.php
+++ resources/views/workload/totalworkload.blade.php
@@ -1,3 +1,3 @@
-<x-layouts.porto title="Total workload per week" header="Total workload per week" username={{$username}}>
+<x-layouts.porto title="Total workload per week" header="Total workload per week" username={{$username}} profile_image={{$profile_image}}>
 <h3>Total word count per week:<strong> {{$totalwordcount}}</h3></strong>
 </x-layouts.porto>
