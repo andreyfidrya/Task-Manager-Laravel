df62222ddad208633662967f5ac629916da57c22
Andrey Fidrya <findhostcoupons@gmail.com>
2025-07-30 16:44:26 +0300

    Notifications for workload

--- resources/views/workload/totalworkload.blade.php
+++ resources/views/workload/totalworkload.blade.php
@@ -1,3 +1,8 @@
-<x-layouts.porto title="Total workload per week" header="Total workload per week" username={{$username}} profile_image={{$profile_image}}>
+<x-layouts.porto title="Notifications" 
+header="Notifications" 
+username={{$username}} 
+profile_image={{$profile_image}} 
+unread_notifications_number={{$unread_notifications_number}} 
+:unread_notifications="$unread_notifications">
 <h3>Total word count per week:<strong> {{$totalwordcount}}</h3></strong>
 </x-layouts.porto>
--- resources/views/workload/workloadperuser.blade.php
+++ resources/views/workload/workloadperuser.blade.php
@@ -1,4 +1,9 @@
-<x-layouts.porto title="Workload per user per week" header="Workload per user per week" username={{$username}} profile_image={{$profile_image}}>
+<x-layouts.porto title="Notifications" 
+header="Notifications" 
+username={{$username}} 
+profile_image={{$profile_image}} 
+unread_notifications_number={{$unread_notifications_number}} 
+:unread_notifications="$unread_notifications">
 @foreach($users as $user)
 <h3>{{$user->name}} has written <strong> {{ $user->tasks()->onlyTrashed()->whereBetween('deleted_at', [$weekStartDate, $weekEndDate])->sum('wordcount'); }} words per week</h3></strong>
 @endforeach
