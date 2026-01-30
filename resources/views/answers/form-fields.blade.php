90c82f3576279f1187ea58d48c1feb7ad5385023
Andrey Fidrya <findhostcoupons@gmail.com>
2026-01-30 21:08:14 +0200

    Edit an Answer (Waiting)

--- resources/views/answers/form-fields.blade.php
+++ resources/views/answers/form-fields.blade.php
@@ -1,7 +1,7 @@
 @csrf
 
 <div class="mb-3">
-    <x-form-select name="Waiting" label="Waiting" :options="['No' => 'No', 'Yes' => 'Yes']" placeholder="Does the answer require waiting?"/> 
+    <x-form-select name="Waiting" label="Waiting" :options="['No' => 'No', 'Please, wait a few minutes. I need to check the information on your request.' => 'Please, wait a few minutes. I need to check the information on your request.', 'Dear customer, I need to check the information on your request. Please, wait a few minutes.' => 'Dear customer, I need to check the information on your request. Please, wait a few minutes.', 'Thank you for the provided information! Please, wait a bit.' => 'Thank you for the provided information! Please, wait a bit.', 'Please, wait a few minutes in the chat.' => 'Please, wait a few minutes in the chat.', 'Пожалуйста, подождите немного. Мне необходимо уточнить информацию по Вашему вопросу.' => 'Пожалуйста, подождите немного. Мне необходимо уточнить информацию по Вашему вопросу.', 'Уважаемый клиент, мне необходимо уточнить информацию по Вашему вопросу. Пожалуйста, подождите несколько минут в чате.' => 'Уважаемый клиент, мне необходимо уточнить информацию по Вашему вопросу. Пожалуйста, подождите несколько минут в чате.', 'Спасибо за предоставленную информацию! Просьба немного подождать.' => 'Спасибо за предоставленную информацию! Просьба немного подождать.', 'Пожалуйста, подождите несколько минут в чате.' => 'Пожалуйста, подождите несколько минут в чате.']" placeholder="Does the answer require waiting?"/> 
 </div>
 <div class="mb-3">
     <x-form-select name="Apologize" label="Apologize" :options="['No' => 'No', 'We apologize for inconvenience' => 'We apologize for inconvenience']" placeholder="Does the answer require apologizes"/> 
