c3aa0f474817410f0aa2edd442f790b82e3a0683
Andrey Fidrya <findhostcoupons@gmail.com>
2026-03-06 19:12:23 +0200

    changes in the form on Edit an Answer page

--- resources/views/answers/form-fields.blade.php
+++ resources/views/answers/form-fields.blade.php
@@ -4,16 +4,7 @@
     <x-form-select 
         name="Waiting" 
         label="Waiting" 
-        :options="[            
-            'Please, wait a few minutes. I need to check the information on your request.' => 'Please, wait a few minutes. I need to check the information on your request.',
-            'Dear customer, I need to check the information on your request. Please, wait a few minutes.' => 'Dear customer, I need to check the information on your request. Please, wait a few minutes.',
-            'Thank you for the provided information! Please, wait a bit.' => 'Thank you for the provided information! Please, wait a bit.',
-            'Please, wait a few minutes in the chat.' => 'Please, wait a few minutes in the chat.',
-            'Пожалуйста, подождите немного. Мне необходимо уточнить информацию по Вашему вопросу.' => 'Пожалуйста, подождите немного. Мне необходимо уточнить информацию по Вашему вопросу.',
-            'Уважаемый клиент, мне необходимо уточнить информацию по Вашему вопросу. Пожалуйста, подождите несколько минут в чате.' => 'Уважаемый клиент, мне необходимо уточнить информацию по Вашему вопросу. Пожалуйста, подождите несколько минут в чате.',
-            'Спасибо за предоставленную информацию! Просьба немного подождать.' => 'Спасибо за предоставленную информацию! Просьба немного подождать.',
-            'Пожалуйста, подождите несколько минут в чате.' => 'Пожалуйста, подождите несколько минут в чате.'
-        ]" 
+        :options="[]" 
         placeholder="No"
     /> 
 </div>
@@ -22,12 +13,11 @@
     <x-form-select 
         name="Apologize" 
         label="Apologize" 
-        :options="['We apologize for inconvenience' => 'We apologize for inconvenience']" 
+        :options="[]" 
         placeholder="No"
     /> 
 </div>
 
-{{-- После Apologize выводим текстовое поле Main Text --}}
 <div class="mb-3">
     <x-form-input 
         name="MainText" 
@@ -40,7 +30,7 @@
     <x-form-select 
         name="Add Question" 
         label="Add Question" 
-        :options="['Is anything else I can help you with?' => 'Is anything else I can help you with?']" 
+        :options="[]" 
         placeholder="No"
     /> 
 </div>
@@ -49,7 +39,7 @@
     <x-form-select 
         name="Goodbye" 
         label="Goodbye" 
-        :options="['If you have any other questions, please contact us' => 'If you have any other questions, please contact us']" 
+        :options="[]" 
         placeholder="No"
     /> 
 </div>
