074ac9cd23eafe7f362eeef3991775fdc4d5e1e3
Andrey Fidrya <findhostcoupons@gmail.com>
2026-04-16 14:56:25 +0300

    Making Changes to CategoryTableSeeder and ScriptTableSeeder

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -12,21 +12,25 @@ class CategoryTableSeeder extends Seeder
     {
         DB::table('categories')->insert([
             [
+                'id' => '1',
                 'name' => 'Waiting sentences',
                 'slug' => 'waiting-sentences',
                 'beforemaintext' => true,
             ], 
             [
+                'id' => '2',
                 'name' => 'Apologize sentences',
                 'slug' => 'apologize-sentences',
                 'beforemaintext' => true,
             ], 
             [
+                'id' => '3',
                 'name' => 'Additional questions',
                 'slug' => 'additional-questions',
                 'beforemaintext' => false,
             ],          
             [
+                'id' => '4',
                 'name' => 'Goodbye sentences',
                 'slug' => 'goodbye-sentences',
                 'beforemaintext' => false,
--- database/seeders/ScriptTableSeeder.php
+++ database/seeders/ScriptTableSeeder.php
@@ -13,19 +13,19 @@ class ScriptTableSeeder extends Seeder
         DB::table('scripts')->insert([
             [
                 'name' => 'Please, wait a few minutes. I need to check the information on your request.',
-                'category_id' => '13',                
+                'category_id' => '1',                
             ], 
             [
                 'name' => 'We apologize for inconvenience',
-                'category_id' => '14',                
+                'category_id' => '2',                
             ], 
             [
                 'name' => 'Is anything else I can help you with?',
-                'category_id' => '15',                
+                'category_id' => '3',                
             ],          
             [
                 'name' => 'If you have any other questions, please contact us',
-                'category_id' => '16',                
+                'category_id' => '4',                
             ],
         ]);
     }
