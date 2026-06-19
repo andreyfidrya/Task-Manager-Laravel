948ce6942cb4b477d62298f757a49b5c004d9454
Andrey Fidrya <findhostcoupons@gmail.com>
2026-06-19 21:01:31 +0300

    +

--- resources/views/answers/form-fields.blade.php
+++ resources/views/answers/form-fields.blade.php
@@ -1,47 +1,48 @@
 
-        @csrf
-        @foreach($categories_before_main_text as $category)
-            <div class="mb-3">
-                <label>{{ $category->name }}</label>
+@csrf
+@foreach($categories_before_main_text as $category)
+    <div class="mb-3">
+        <label>{{ $category->name }}</label>
 
-                <select name="{{ $category->slug }}" class="form-select">
-                    <option value="">No</option>
+        <select name="{{ $category->slug }}" class="form-select">
+            <option value="">No</option>
 
-                    @foreach($category->scripts as $script)
-                        <option
-                            value="{{ $script->name }}"
-                            @selected(old($category->slug, $answer->{$category->slug}) == $script->name)
-                        >
-                            {{ $script->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-        @endforeach
+            @foreach($category->scripts as $script)
+                <option
+                    value="{{ $script->name }}"
+                    @selected(old($category->slug, $answer->{$category->slug}) == $script->name)
+                >
+                    {{ $script->name }}
+                </option>
+            @endforeach
+        </select>
 
-        <input
-            type="text"
-            name="maintext"
-            class="form-control"
-            value="{{ old('maintext', $answer->maintext) }}"
-        >
+    </div>
+@endforeach
 
-        @foreach($categories_after_main_text as $category)
-            <div class="mb-3">
-                <label>{{ $category->name }}</label>
+<input
+    type="text"
+    name="maintext"
+    class="form-control"
+    value="{{ old('maintext', $answer->maintext) }}"
+>
 
-                <select name="{{ $category->slug }}" class="form-select">
-                    <option value="">No</option>
+@foreach($categories_after_main_text as $category)
+    <div class="mb-3">
+        <label>{{ $category->name }}</label>
 
-                    @foreach($category->scripts as $script)
-                        <option
-                            value="{{ $script->name }}"
-                            @selected(old('addquestion', $answer->addquestion) == $script->name)
-                        >
-                            {{ $script->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-        @endforeach
+        <select name="{{ $category->slug }}" class="form-select">
+            <option value="">No</option>
+
+            @foreach($category->scripts as $script)
+                <option
+                    value="{{ $script->name }}"
+                    @selected(old('addquestion', $answer->addquestion) == $script->name)
+                >
+                    {{ $script->name }}
+                </option>
+            @endforeach
+        </select>
+    </div>
+@endforeach
         
